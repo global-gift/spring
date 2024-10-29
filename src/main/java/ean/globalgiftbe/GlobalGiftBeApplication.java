4e42ada109544350a0f4f5c290db4299845818a7
Dingwoonee <slm0523@naver.com>
2024-10-30 00:27:43 +0900

    first commit

---
+++ src/main/java/ean/globalgiftbe/GlobalGiftBeApplication.java
@@ -0,0 +1,13 @@
+package ean.globalgiftbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalGiftBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalGiftBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ean/globalgiftbe/GlobalGiftBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ean.globalgiftbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalGiftBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
